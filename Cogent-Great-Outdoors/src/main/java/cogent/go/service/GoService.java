4e66fc08a0741429805b586323bfceae2499ddcf
jayja <jayja@Jaycred>
2021-08-03 18:08:43 -0400

    Consolidated all services into one and all controllers into one

---
+++ Cogent-Great-Outdoors/src/main/java/cogent/go/service/GoService.java
@@ -0,0 +1,54 @@
+package cogent.go.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cogent.go.dao.CartRepository;
+import cogent.go.dao.CustomerQueryRepository;
+import cogent.go.dao.DeliveryAddressRepository;
+import cogent.go.dao.OrderRepository;
+import cogent.go.dao.ProductRepository;
+import cogent.go.dao.UserRepository;
+import cogent.go.entities.Cart;
+import cogent.go.entities.CustomerQuery;
+import cogent.go.entities.DeliveryAddress;
+import cogent.go.entities.Order;
+import cogent.go.entities.Product;
+import cogent.go.entities.User;
+
+@Service
+public class GoService {
+	
+	@Autowired
+	private ProductRepository productRep;
+	@Autowired
+	private CartRepository cartRep;
+	@Autowired
+	private UserRepository userRep;
+	@Autowired
+	private CustomerQueryRepository custQueryRep;
+	@Autowired
+	private DeliveryAddressRepository addressRep;
+	@Autowired
+	private OrderRepository orderRep;
+	
+	public Product saveProduct(Product product) {
+        return productRep.save(product);
+    }
+	public Cart saveCart(Cart cart) {
+        return cartRep.save(cart);
+    }
+	public CustomerQuery saveQuery(CustomerQuery query) {
+        return custQueryRep.save(query);
+    }
+	public Order saveOrder(Order order) {
+        return orderRep.save(order);
+    }
+	public DeliveryAddress saveAddress(DeliveryAddress address) {
+        return addressRep.save(address);
+    }
+	public User saveUser(User user) {
+        return userRep.save(user);
+    }
+	
+}
