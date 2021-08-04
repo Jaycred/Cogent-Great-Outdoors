4ac9d70a84694e4d7321f180162e9120ad18368f
jayja <jayja@Jaycred>
2021-08-04 15:12:13 -0400

    test

--- Cogent-Go-Frontend/src/app/app.module.ts
+++ Cogent-Go-Frontend/src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+const route: Routes = [
+  /*{path: 'products/category/:cid', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: 'products/add', component: ProductAddComponent},
+  {path: 'products/addT', component: ProductAddTComponent},
+  {path: 'products/:id', component: ProductDetailsComponent},
+  {path: 'search/:keyword', component: ProductListComponent},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}*/
+];
 
 @NgModule({
   declarations: [
