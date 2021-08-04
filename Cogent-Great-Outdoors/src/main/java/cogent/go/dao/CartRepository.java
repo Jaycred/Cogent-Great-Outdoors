a65307553ec1467a96bcfc3c5a911023985b827b
HenryAndJames <88346627+HenryAndJames@users.noreply.github.com>
2021-08-04 16:28:38 -0700

    Added repository annotations

--- Cogent-Great-Outdoors/src/main/java/cogent/go/dao/CartRepository.java
+++ Cogent-Great-Outdoors/src/main/java/cogent/go/dao/CartRepository.java
@@ -1,11 +1,13 @@
 package cogent.go.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import cogent.go.entities.Cart;
 
 @CrossOrigin()
+@Repository
 public interface CartRepository extends JpaRepository<Cart, Integer> {
 
 }
--- Cogent-Great-Outdoors/src/main/java/cogent/go/dao/ProductRepository.java
+++ Cogent-Great-Outdoors/src/main/java/cogent/go/dao/ProductRepository.java
@@ -3,12 +3,14 @@ package cogent.go.dao;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import cogent.go.entities.Product;
 
 @CrossOrigin()
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 	Page<Product> findByCategory(@RequestParam("id") String id, Pageable pageable);
 
