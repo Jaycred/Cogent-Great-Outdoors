7661f39f6fc59a9b6a8069c7aeaa16f49b12ad84
HenryAndJames <88346627+HenryAndJames@users.noreply.github.com>
2021-08-04 13:29:00 -0700

    Commit

--- Cogent-Go-Frontend/src/app/common/cart.ts
+++ Cogent-Go-Frontend/src/app/common/cart.ts
@@ -1,12 +1,10 @@
-import { DeliveryAddress } from "./delivery-address";
 import { Product } from "./product";
 import { User } from "./user";
 
-export class Order {
-    orderId:number;
+export class Cart {
+    cartId:number;
 	user:User;
     product: Product;
 	quantity: number;
 	price:number;
-    da:DeliveryAddress
 }
