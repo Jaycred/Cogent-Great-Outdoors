df7409061bf075b1272f9bd2e189fed8d4a5473d
HenryAndJames <88346627+HenryAndJames@users.noreply.github.com>
2021-08-04 17:22:50 -0700

    commit

--- Cogent-Go-Frontend/src/app/app.module.ts
+++ Cogent-Go-Frontend/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductAddComponentComponent } from './components/product-add-component/product-add-component.component';
+import { AddressAddComponentComponent } from './components/address-add-component/address-add-component.component';
+import { UserAddComponentComponent } from './components/user-add-component/user-add-component.component';
+import { CartAddComponentComponent } from './components/cart-add-component/cart-add-component.component';
+import { QueryAddComponentComponent } from './components/query-add-component/query-add-component.component';
+import { OrderAddComponentComponent } from './components/order-add-component/order-add-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductAddComponentComponent
+    ProductAddComponentComponent,
+    AddressAddComponentComponent,
+    UserAddComponentComponent,
+    CartAddComponentComponent,
+    QueryAddComponentComponent,
+    OrderAddComponentComponent
   ],
   imports: [
     BrowserModule
--- Cogent-Go-Frontend/src/app/services/go-service.service.ts
+++ Cogent-Go-Frontend/src/app/services/go-service.service.ts
@@ -24,6 +24,31 @@ export class GoServiceService {
     return this.httpClient.post<MessageResponse>(url,p,this.httpOptions).pipe(map(response => response.result));
   }
 
+  addAddress(da:any): Observable<string> {
+    const url = this.baseUrl+"addAddress";
+    return this.httpClient.post<MessageResponse>(url,da,this.httpOptions).pipe(map(response => response.result));
+  }
+
+  addOrder(order:any): Observable<string> {
+    const url = this.baseUrl+"placeOrder";
+    return this.httpClient.post<MessageResponse>(url,order,this.httpOptions).pipe(map(response => response.result));
+  }
+
+  addUser(user:any): Observable<string> {
+    const url = this.baseUrl+"addUser";
+    return this.httpClient.post<MessageResponse>(url,user,this.httpOptions).pipe(map(response => response.result));
+  }
+
+  addQuery(query:any): Observable<string> {
+    const url = this.baseUrl+"createQuery";
+    return this.httpClient.post<MessageResponse>(url,query,this.httpOptions).pipe(map(response => response.result));
+  }
+
+  addCart(cart:any): Observable<string> {
+    const url = this.baseUrl+"saveCart";
+    return this.httpClient.post<MessageResponse>(url,cart,this.httpOptions).pipe(map(response => response.result));
+  }
+
 }
 
 interface MessageResponse{  
