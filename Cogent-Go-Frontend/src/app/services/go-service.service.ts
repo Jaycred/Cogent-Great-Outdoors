11d0d124bb42dff7ea15c5aad75676cd57b47b81
HenryAndJames <88346627+HenryAndJames@users.noreply.github.com>
2021-08-05 11:15:20 -0700

    commit

--- Cogent-Go-Frontend/src/app/services/go-service.service.ts
+++ Cogent-Go-Frontend/src/app/services/go-service.service.ts
@@ -24,10 +24,6 @@ export class GoServiceService {
     return this.httpClient.post<MessageResponse>(url,p,this.httpOptions).pipe(map(response => response.result));
   }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> branch 'main' of https://github.com/Jaycred/Cogent-Great-Outdoors.git
   addAddress(da:any): Observable<string> {
     const url = this.baseUrl+"addAddress";
     return this.httpClient.post<MessageResponse>(url,da,this.httpOptions).pipe(map(response => response.result));
@@ -51,17 +47,8 @@ export class GoServiceService {
   addCart(cart:any): Observable<string> {
     const url = this.baseUrl+"saveCart";
     return this.httpClient.post<MessageResponse>(url,cart,this.httpOptions).pipe(map(response => response.result));
-<<<<<<< HEAD
-=======
-  placeOrder(o:any): Observable<string> {
-    const url = this.baseUrl+"placeOrder/";
-    return this.httpClient.post<MessageResponse>(url,o,this.httpOptions).pipe(map(response => response.result));
->>>>>>> branch 'main' of https://github.com/Jaycred/Cogent-Great-Outdoors.git
-=======
 
->>>>>>> branch 'main' of https://github.com/Jaycred/Cogent-Great-Outdoors.git
-  }
-
+}
 }
 
 interface MessageResponse{  
