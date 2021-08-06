8151a290991da2a9093326081ca91e4769ccfd07
HenryAndJames <88346627+HenryAndJames@users.noreply.github.com>
2021-08-05 18:55:49 -0700

    Commit

--- Cogent-Go-Frontend/src/app/services/go-service.service.ts
+++ Cogent-Go-Frontend/src/app/services/go-service.service.ts
@@ -58,12 +58,12 @@ export class GoServiceService {
 
   getProductsById(id: number): Observable<Product[]>
   {
-    const url = '${this.baseUrl}/findProductsById?id=${id}';
+    const url = `${this.baseUrl}/findProductsById/${id}`;
     return this.httpClient.get<Product[]>(url);
   }
   getProductsByCategory(cName: string): Observable<Product[]>
   {
-    const url = '${this.baseUrl}/findProductsByCategory?category=${cName}';
+    const url = `${this.baseUrl}/findProductsByCategory/${cName}`;
     return this.httpClient.get<Product[]>(url);
   }
 }
