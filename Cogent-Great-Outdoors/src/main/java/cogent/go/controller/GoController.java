f766fe8a823d996ff38394357f8d465b644cf92e
James Jasinski <37227414+Jaycred@users.noreply.github.com>
2021-08-07 15:54:21 -0400

    Update GoController.java

--- Cogent-Great-Outdoors/src/main/java/cogent/go/controller/GoController.java
+++ Cogent-Great-Outdoors/src/main/java/cogent/go/controller/GoController.java
@@ -1,11 +1,23 @@
 package cogent.go.controller;
 
+import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,21 +26,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import cogent.go.dao.UserRepository;
 import cogent.go.entities.Cart;
 import cogent.go.entities.CustomerQuery;
 import cogent.go.entities.DeliveryAddress;
 import cogent.go.entities.Order;
 import cogent.go.entities.Product;
 import cogent.go.entities.User;
+import cogent.go.security.config.JwtTokenUtil;
+import cogent.go.security.model.JwtRequest;
+import cogent.go.security.model.JwtResponse;
+import cogent.go.security.model.MessageResponse;
+import cogent.go.security.model.UserDetailsImpl;
 import cogent.go.service.GoService;
 
-@CrossOrigin
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/go")
 public class GoController {
 	
 	@Autowired
     private GoService service;
+	
+	@Autowired
+	AuthenticationManager authenticationManager;
+	
+	@Autowired
+	JwtTokenUtil jwtUtils;
+	
+	@Autowired
+	PasswordEncoder encoder;
+	
+	@Autowired
+	UserRepository userRepository;
 	
 	@PostMapping("/addProduct")
     public ResponseEntity<String> addProduct(@RequestBody Product product) {
@@ -68,19 +98,6 @@ public class GoController {
         return new ResponseEntity<>("Cart #" + cart.getCartId() + " was saved.", HttpStatus.OK);
     }
 	
-	@GetMapping("/findAllCarts")
-	public List<Cart> getCartList(){
-		return service.getCartList();
-	}
-	@GetMapping("/findCartsById")
-	public Optional<Cart> getCartById(@RequestParam("id") int id){
-		return service.getCartById(id);
-	}
-	@GetMapping("/findCartsByUserId")
-	public List<Cart> getCartByUserId(@RequestParam("id") int id){
-		return service.getCartByUserId(id);
-	}
-	
 	@GetMapping("/findAllProducts")
 	public List<Product> getProductList(){
 		return service.getProductList();
@@ -91,13 +108,49 @@ public class GoController {
 	}
 	@GetMapping("/findProductsById")
 	public List<Product> getProductsById(@RequestParam("id") int id){
+
 		return service.getProductById(id);
 	}
+	
+	
 	@PostMapping("/login")
-    public User login(@RequestParam("email") String email, @RequestParam("password") String password) {
-        return service.login(email, password);
-    }
+	public ResponseEntity<?> authenticateUser(@Valid @RequestBody JwtRequest loginRequest) {
+
+		Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		String jwt = jwtUtils.generateJwtToken(authentication);
+		
+		UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+		return ResponseEntity.ok(new JwtResponse(jwt, 
+												 userDetails.getId(),
+												 userDetails.getEmail()));
+	}
+
+	@PostMapping("/signup")
+	public ResponseEntity<?> registerUser(@Valid @RequestBody JwtRequest signUpRequest) {
+		if (userRepository.findByEmail(signUpRequest.getEmail()).isPresent()) {
+			return ResponseEntity
+					.badRequest()
+					.body(new MessageResponse("Error: Username/email is already taken!"));
+		}
+
+		////////////////////////////FIX///////////////////////////////
+		// Create new user's account
+		User user = new User(signUpRequest.getFirstName(), signUpRequest.getLastName(), signUpRequest.getPhoneNumber(),
+							signUpRequest.getEmail(), encoder.encode(signUpRequest.getPassword()),
+							signUpRequest.getAddressLine1(), signUpRequest.getAddressLine2(), signUpRequest.getState(),
+							signUpRequest.getPincode());
+
+		//////////////////////////////FIX/////////////////////////////////////////
+
+		service.saveUser(user);
+		// mail service to send plain text mail to user's email account about successful
+		// registration
+		return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+		
+		}
 	
-	
-	// please work
 }
