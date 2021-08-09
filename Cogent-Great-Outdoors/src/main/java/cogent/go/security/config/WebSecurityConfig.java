9e4405dc27cd39fcdc449e5907e73d21e43eeae8
James Jasinski <37227414+Jaycred@users.noreply.github.com>
2021-08-09 16:37:07 -0400

    j

--- Cogent-Great-Outdoors/src/main/java/cogent/go/security/config/WebSecurityConfig.java
+++ Cogent-Great-Outdoors/src/main/java/cogent/go/security/config/WebSecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(
+		//
 		// securedEnabled = true,
 		// jsr250Enabled = true,
 		prePostEnabled = true)
