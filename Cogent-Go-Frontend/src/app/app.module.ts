3c1648b151bc61bf8bbe1c9f7544b7fca9a5c50e
HenryAndJames <88346627+HenryAndJames@users.noreply.github.com>
2021-08-09 12:58:07 -0700

    Commit

--- Cogent-Go-Frontend/src/app/app.module.ts
+++ Cogent-Go-Frontend/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductAddComponentComponent } from './components/product-add-component/product-add-component.component';
 import { AddressAddComponentComponent } from './components/address-add-component/address-add-component.component';
-import { UserAddComponentComponent } from './components/user-add-component/user-add-component.component';
 import { QueryAddComponentComponent } from './components/query-add-component/query-add-component.component';
 import { OrderAddComponentComponent } from './components/order-add-component/order-add-component.component';
 import { ProductViewComponent } from './components/product-view/product-view.component';
@@ -16,25 +15,22 @@ import { CategoryViewComponent } from './components/category-view/category-view.
 import { ProductSpecsComponent } from './components/product-specs/product-specs.component';
 import { CartViewComponent } from './components/cart-view/cart-view.component';
 import { UserAccountComponent } from './components/user-account/user-account.component';
-import { UserLoginComponent } from './components/user-login/user-login.component';
-import { SecurityServiceComponent } from './security-service/security-service.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { ProfileComponent } from './profile/profile.component';
-
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 const route: Routes = [
   {path: 'categories/:categoryName', component: ProductViewComponent},
   {path: 'categories', component: CategoryViewComponent},
   {path: 'products/:id', component: ProductSpecsComponent},
   {path: 'products', component: ProductViewComponent},
-  {path: 'signup', component: UserAddComponentComponent},
   {path: 'addProduct', component: ProductAddComponentComponent},
-  {path: 'addCart', component: CartAddComponentComponent},
-  {path: 'login', component: UserLoginComponent},
   {path: 'cart', component: CartViewComponent},
   {path: 'cart/:cartId', component:CartViewComponent},
   {path: 'cart/user/:userId', component:CartViewComponent},
+  {path: 'signup', component: RegisterComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'token', component:ProfileComponent},
   {path: '**', redirectTo: 'categories', pathMatch: 'full'}
 ];
 
@@ -43,7 +39,6 @@ const route: Routes = [
     AppComponent,
     ProductAddComponentComponent,
     AddressAddComponentComponent,
-    UserAddComponentComponent,
     QueryAddComponentComponent,
     OrderAddComponentComponent,
     ProductViewComponent,
@@ -51,8 +46,6 @@ const route: Routes = [
     ProductSpecsComponent,
     CartViewComponent,
     UserAccountComponent,
-    UserLoginComponent,
-    SecurityServiceComponent,
     LoginComponent,
     RegisterComponent,
     ProfileComponent
