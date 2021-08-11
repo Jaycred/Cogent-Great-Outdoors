f6ee6f051322ed26955d32f6575119c31712941d
James Jasinski <37227414+Jaycred@users.noreply.github.com>
2021-08-11 15:18:30 -0400

    Update auth-interceptor.ts

--- Cogent-Go-Frontend/src/app/_helpers/auth-interceptor.ts
+++ Cogent-Go-Frontend/src/app/_helpers/auth-interceptor.ts
@@ -1,19 +1,18 @@
 import { HTTP_INTERCEPTORS, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
-
-import { GoServiceService } from '../services/go-service.service';
 import { Observable } from 'rxjs';
+import { GoServiceService } from '../services/go-service.service';
 
 const TOKEN_HEADER_KEY = 'Authorization';       // for Spring Boot back-end
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private gs: GoServiceService) { }
+  constructor(private token: GoServiceService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let authReq = req;
-    const token = this.gs.getToken();
+    const token = this.token.getToken();
     if (token != null) {
       authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
     }
